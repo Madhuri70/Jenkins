f4a18e6c93403fe745afb3a7dae0685b7c88dc7f
Madhuri <thiragabathinamadhuri@gmail.com>
2024-02-26 09:46:14 +0530

    firstcommit

---
+++ PhaseJenkins/PizzaManagement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhaseJenkins
+{
+    public class PizzaManagement
+    {
+        static List<Pizza> pizzas = new List<Pizza>
+        {
+            new Pizza { Id =1, Name="Panner Pizza",Price=600},
+            new Pizza { Id =2, Name="Mushroom Pizza",Price=750},
+
+        }; public List<Pizza> AllPizzas()
+        {
+            return pizzas;
+        }
+    }
+}
+
---
+++ PhaseJenkins/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security;
+using PhaseJenkins;
+
+
+    Console.WriteLine("Joe's Pizza detals!!");
+PizzaManagement pm = new PizzaManagement();
+foreach (var p in pm.AllPizzas())
+{
+    Console.WriteLine("Id:" + p.Id);
+    Console.WriteLine("Name:" + p.Name);
+    Console.WriteLine("Price:" + p.Price);
+}
